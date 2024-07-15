7e754919ff4b62fd269ddd6627c06dcff650054a
semajssor <141041402+semajssor@users.noreply.github.com>
2024-07-15 17:51:16 +0300

    resolving the babel issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   experimental: {
+    swcLoader: {
+      jsc: {
+        transform: {
+          react: {
+            runtime: 'automatic',
+          },
+        },
+        externalHelpers: true,
+        plugin: [
+          // Include your Babel plugins here
+        ],
+      },
+    },
+  },};
 
 export default nextConfig;
